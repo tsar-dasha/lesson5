bdd70dba6b88ad51fe15188d8e1e9acef802185d
Tsareva Darya <d.tsareva@space307.com>
2024-07-14 17:59:50 +0200

    added fix to enterprise test

--- src/test/java/TestEnterprise.java
+++ src/test/java/TestEnterprise.java
@@ -2,7 +2,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -20,6 +20,6 @@ public class TestEnterprise {
         open("https://github.com");
         $(byText("Solutions")).hover();
         $(byTagAndText("a","Enterprise")).click();
-        $("The AI-powered developer platform.").shouldBe(visible);
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" + "developer platform."));
     }
 }
