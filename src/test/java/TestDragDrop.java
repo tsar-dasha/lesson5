06653e10e9f10508697a78b2e9fca9c3ac403282
Tsareva Darya <d.tsareva@space307.com>
2024-07-16 21:45:17 +0200

    added fix EnterpriseTest

--- src/test/java/TestDragDrop.java
+++ src/test/java/TestDragDrop.java
@@ -14,11 +14,22 @@ public class TestDragDrop {
     }
 
     @Test
-    void moveRectangle(){
+    void moveRectangleTest(){
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(220, 0).release().perform();
         $("#column-a").shouldHave(text("B"));
-        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void moveRectangleActionsTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 }
--- src/test/java/TestEnterprise.java
+++ src/test/java/TestEnterprise.java
@@ -1,24 +1,25 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.commands.ShouldHave;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+
 
 public class TestEnterprise {
     @BeforeAll
             static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-
     }
+
     @Test
-    void openEnterprise(){
+    void openEnterpriseTest(){
         open("https://github.com");
-        $(byText("Solutions")).hover();
-        $(byTagAndText("a","Enterprise")).click();
-        $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" + "developer platform."));
+       $(".Header-old").$(byText("Solutions")).hover();
+       $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+       $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" + "developer platform."));
     }
 }
